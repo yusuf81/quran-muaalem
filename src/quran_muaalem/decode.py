9d963b2142894639c9520d5633a8024bcf6459f3
obadx <abdullahamlyossef@gmail.com>
2025-08-25 01:08:16 +0300

    bug: typo

--- src/quran_muaalem/decode.py
+++ src/quran_muaalem/decode.py
@@ -494,8 +494,8 @@ def multilevel_greedy_decode(
             # we slove this by two steps
             # 1. Align the sifa level with length mismatch to the refrence sifa level
             # 2. align the alinged sifa level back to the the length of prediced phonmes
-            if len(decode_out.ids) != len(chunked_phonemes_batch[seq_idx]) and len(
-                chunked_phonemes_batch[seq_idx]
+            if len(decode_out.ids) != len(chunked_phonemes_batch[seq_idx]) and (
+                len(chunked_phonemes_batch[seq_idx])
                 <= len(ref_chuncked_phonemes_batch[seq_idx])
             ):
                 logging.info(f"Sequence: `{seq_idx}` has mismatch Level: {level}")
