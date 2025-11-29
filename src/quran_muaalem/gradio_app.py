bbdb3eb85d4660a98af7381465fff0ee59698954
yusuf81 <syafroni@aol.com>
2025-11-29 17:15:09 +0700

    ubah menambahkan tab tentang dan credit di aplikasi gradio

--- src/quran_muaalem/gradio_app.py
+++ src/quran_muaalem/gradio_app.py
@@ -965,6 +965,82 @@ with gr.Blocks(title="Pengajar Al-Quran") as app:
             reset_settings, inputs=[], outputs=settings_components + [status_message]
         )
 
+    with gr.Tab("Tentang & Credit"):
+        gr.Markdown("""
+        # Tentang Aplikasi - About This Application
+        
+        Aplikasi ini menggunakan model **Muaalem** untuk deteksi kesalahan dan koreksi pengucapan dalam bacaan Al-Quran berdasarkan kaidah tajwid.
+        
+        *This application uses the **Muaalem** model for pronunciation error detection and correction in Quranic recitation based on tajweed rules.*
+        
+        ---
+        
+        ## 📄 Paper / Makalah Penelitian
+        
+        **Title:** Automatic Pronunciation Error Detection and Correction of the Holy Quran's Learners Using Deep Learning
+        
+        **Authors:** Abdullah Abdelfattah, Mahmoud I. Khalil, Hazem Abbas
+        
+        **Published:** arXiv preprint arXiv:2509.00094 (2025)
+        
+        **Links:**
+        - 📖 [Read Paper on arXiv](https://arxiv.org/abs/2509.00094)
+        - 🌐 [Project Website](https://obadx.github.io/prepare-quran-dataset/)
+        - 📓 [PDF Version](https://arxiv.org/pdf/2509.00094)
+        - 🔗 [DOI: 10.48550/arXiv.2509.00094](https://doi.org/10.48550/arXiv.2509.00094)
+        
+        ---
+        
+        ## 💻 Source Code / Kode Sumber
+        
+        **GitHub Repository:** [obadx/quran-muaalem](https://github.com/obadx/quran-muaalem)
+        
+        ---
+        
+        ## 📚 Citation / Sitasi
+        
+        Jika Anda menggunakan aplikasi ini atau model Muaalem dalam penelitian Anda, mohon sitasi paper berikut:
+        
+        *If you use this application or the Muaalem model in your research, please cite the following paper:*
+        
+        ### BibTeX
+        
+        ```bibtex
+        @article{abdelfattah2025automatic,
+          title={Automatic Pronunciation Error Detection and Correction of the Holy Quran's Learners Using Deep Learning},
+          author={Abdelfattah, Abdullah and Khalil, Mahmoud I. and Abbas, Hazem},
+          journal={arXiv preprint arXiv:2509.00094},
+          year={2025},
+          url={https://arxiv.org/abs/2509.00094},
+          doi={10.48550/arXiv.2509.00094}
+        }
+        ```
+        
+        ### APA Style
+        
+        ```
+        Abdelfattah, A., Khalil, M. I., & Abbas, H. (2025). Automatic Pronunciation Error 
+        Detection and Correction of the Holy Quran's Learners Using Deep Learning. 
+        arXiv preprint arXiv:2509.00094. https://doi.org/10.48550/arXiv.2509.00094
+        ```
+        
+        ---
+        
+        ## 🙏 Acknowledgments / Penghargaan
+        
+        Terima kasih kepada tim peneliti yang telah mengembangkan model Muaalem dan menyediakan dataset serta kode sumber secara open-source.
+        
+        *Thanks to the research team who developed the Muaalem model and provided the dataset and source code as open-source.*
+        
+        ---
+        
+        ## ⚖️ License / Lisensi
+        
+        Proyek ini menggunakan lisensi yang sama dengan repository aslinya. Silakan kunjungi [GitHub repository](https://github.com/obadx/quran-muaalem) untuk informasi lisensi lengkap.
+        
+        *This project uses the same license as the original repository. Please visit the [GitHub repository](https://github.com/obadx/quran-muaalem) for complete license information.*
+        """)
+
 
 def main(app=app):
     root_path = os.environ.get("GRADIO_ROOT_PATH", "/ngaji7")
